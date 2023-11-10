b37328700b433b8c8c3b7ecc783e450bc54eb7ae
A2s3ne <124436569+A2s3ne@users.noreply.github.com>
2023-11-10 20:50:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ if (isDaytime(now)) {
     document.body.style.color = "#333";
 } else {
     // Nighttime
-    document.body.style.background = "#f2f2f2";
-    document.body.style.color = "#262626";
+    document.body.style.background = "#262626";
+    document.body.style.color = "#f2f2f2";
 }
 
 function countdown() {
