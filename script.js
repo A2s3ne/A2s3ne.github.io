c00c868eb23e5e08a46fa1341358ef1d7c8939bb
A2s3ne <124436569+A2s3ne@users.noreply.github.com>
2023-11-10 20:14:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,25 @@ const daysEl = document.getElementById("days");
 const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
-
 const newYears = "27 Nov 2023";
+
+function isDaytime(dateStr) {
+ const date = new Date(dateStr);
+ return date.getHours() >= 5 && date.getHours() < 18;
+}
+
+const now = new Date();
+const hours = now.getHours();
+
+if (isDaytime(hours)) {
+ // Daytime
+ document.body.style.background = "#f2f2f2";
+ document.body.style.color = "#333";
+} else {
+ // Nighttime
+ document.body.style.background = "#333";
+ document.body.style.color = "#f2f2f2";
+}
 
 function countdown() {
     const newYearsDate = new Date(newYears);
