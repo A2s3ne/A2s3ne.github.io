126ac738912a7034b4e72500237aefd681e88516
A2s3ne <124436569+A2s3ne@users.noreply.github.com>
2023-12-03 15:34:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,43 +5,50 @@ const secondsEl = document.getElementById("seconds");
 const newYears = "2023-12-15T11:55:00";
 
 function isDaytime(dateStr) {
-const date = new Date(dateStr);
-return date.getHours() >= 5 && date.getHours() < 18;
+    const date = new Date(dateStr);
+    return date.getHours() >= 5 && date.getHours() < 18;
 }
 
 const now = new Date();
 const hours = now.getHours();
 
 if (isDaytime(now)) {
-// Daytime
-document.body.style.background = "##ff0000";
-document.body.style.color = "#333";
+    // Daytime
+    document.body.style.background = "##ff0000";
+    document.body.style.color = "#333";
 } else {
-// Nighttime
-document.body.style.background = "#262626";
-document.body.style.color = "#f2f2f2";
+    // Nighttime
+    document.body.style.background = "#262626";
+    document.body.style.color = "#f2f2f2";
 }
 
 function countdown() {
-const newYearsDate = new Date(newYears);
-const currentDate = new Date()
-const totalSeconds = (newYearsDate - currentDate) / 1000;
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
 
-const days = Math.floor(totalSeconds / 3600 / 24);
-const hours = Math.floor(totalSeconds / 3600) % 24;
-const mins = Math.floor(totalSeconds / 60) % 60;
-const seconds = Math.floor(totalSeconds) % 60;
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-daysEl.innerHTML = days;
-hoursEl.innerHTML = formatTime(hours);
-minsEl.innerHTML = formatTime(mins);
-secondsEl.innerHTML = formatTime(seconds);
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
 
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
 }
 
 function formatTime(time) {
-return time < 10 ? 0${time} : time;
+    return time < 10 ? `0${time}` : time;
 }
 
+// initial call
 countdown();
+
 setInterval(countdown, 1000);
+if (diff > 0) {
+    setTimeout(() => {
+        countdown();
+    }, diff);
+}
