f178dd9103c799a8a90f6fc2a6ebcfe330e3a241
A2s3ne <124436569+A2s3ne@users.noreply.github.com>
2023-12-03 15:44:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const daysEl = document.getElementById("days");
 const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
-const newYears = "2023-12-15T11:55:00";
+const newYears = "2023-12-03T16:24:03";
 
 function isDaytime(dateStr) {
     const date = new Date(dateStr);
@@ -14,11 +14,11 @@ const hours = now.getHours();
 
 if (isDaytime(now)) {
     // Daytime
-    document.body.style.background = "#f2f2f2";
-    document.body.style.color = "#2b2b2b";
+    document.body.style.background = "##ff0000";
+    document.body.style.color = "#333";
 } else {
     // Nighttime
-    document.body.style.background = "#212121";
+    document.body.style.background = "#262626";
     document.body.style.color = "#f2f2f2";
 }
 
@@ -47,3 +47,14 @@ function formatTime(time) {
 countdown();
 
 setInterval(countdown, 1000);
+
+// Update the countdown to start at a specific time
+const targetTime = new Date("2023-11-27T15:00:00"); // 3 PM on November 27th, 2023
+const currentTime = new Date();
+const diff = targetTime - currentTime;
+
+if (diff > 0) {
+    setTimeout(() => {
+        countdown();
+    }, diff);
+}
